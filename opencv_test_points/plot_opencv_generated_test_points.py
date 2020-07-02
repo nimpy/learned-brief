06b21da83b773ef41a2cca8b983e938cc320a28c
niaki <nina.zizakic@ugent.be>
2020-07-02 11:59:07 +0200

    Beautify the code

--- opencv_test_points/plot_opencv_generated_test_points.py
+++ opencv_test_points/plot_opencv_generated_test_points.py
@@ -1,12 +1,18 @@
+"""
+Plotting the random points used for binary pixel tests in BRIEF OpenCV implementation.
+They should be picked from a Gaussian distribution around the centre of the patch, 
+as described in the original BRIEF paper.
+
+Points taken from:
+https://github.com/opencv/opencv_contrib/blob/4.3.0/modules/xfeatures2d/src/generated_16.i
+https://github.com/opencv/opencv_contrib/blob/4.3.0/modules/xfeatures2d/src/generated_32.i
+https://github.com/opencv/opencv_contrib/blob/4.3.0/modules/xfeatures2d/src/generated_64.i
+
+"""
 import matplotlib.pyplot as plt
 
-# Plotting the random "testing" points that the OpenCV library uses to check if they are picked from a Gaussian distribution.
-# Points taken from:
-# https://github.com/opencv/opencv_contrib/blob/master/modules/xfeatures2d/src/generated_16.i
-# https://github.com/opencv/opencv_contrib/blob/master/modules/xfeatures2d/src/generated_64.i
 
-
-file1 = open('generated_64.txt', 'r') 
+file1 = open('generated_16.txt', 'r') 
 count = 0
 
 
@@ -27,10 +33,11 @@ while True:
     print(x_coord, y_coord)
 file1.close() 
 
-
+plt.figure(figsize=(8,8))
 plt.scatter(x_coords, y_coords)
 plt.show()
 
+plt.figure(figsize=(8,8))
 for i in range(0, len(x_coords), 2):
     plt.plot(x_coords[i:i+2], y_coords[i:i+2], 'ro-')
 
---
+++ step1_coordinates_to_dense_layer_weights.py
@@ -0,0 +1,77 @@
+"""
+Generating weights for the dense layer of the neural network that should perform binary pixel tests as described in BRIEF paper. 
+
+The weights are generated based on the list of pixel coordinates counted from the patch centre (so in range [-23..24]). 
+
+The lists of pixel coordinates are taken from OpenCV Github repo files:
+https://github.com/opencv/opencv_contrib/blob/4.3.0/modules/xfeatures2d/src/generated_16.i
+https://github.com/opencv/opencv_contrib/blob/4.3.0/modules/xfeatures2d/src/generated_32.i
+https://github.com/opencv/opencv_contrib/blob/4.3.0/modules/xfeatures2d/src/generated_64.i
+
+The coordinates are extracted from those files and are under:
+./opencv_test_points/generated_X.txt
+where X can be 16, 32, or 64. These represent different versions of BRIEF taking X bytes.
+The number of keypoints in the files is X * 8 * 2 (8 because it's bytes, and 2 because it's pairs of keypoints).
+
+"""
+
+import numpy as np
+
+
+def generate_and_save_dense_layer_weights():
+
+	brief_patch_size = 48  # PATCH_SIZE variable hard-coded into OpenCV BRIEF (see OpenCV repo)
+	centre_coord_offset = 23  # hard-coded into OpenCV BRIEF, around the half of the PATCH_SIZE as defined there
+
+	brief_version = 64  # either 16, 32, or 64
+
+    # properties of the fully-connected layer
+	input_dimension = brief_patch_size * brief_patch_size
+	output_dimension = brief_version * 8
+
+	file = open('./opencv_test_points/generated_' + str(brief_version) + '.txt', 'r') 
+
+	x_coord = 0
+	y_coord = 0
+
+	output_count = 0
+	total_count = 0
+
+	weights = np.zeros((input_dimension, output_dimension), dtype = np.float32)
+
+	while True:
+		line = file.readline()
+		if not line: 
+		    break
+		
+		x_coord_prev = x_coord
+		y_coord_prev = y_coord
+		
+		x_coord = int(line[1: line.find(",")])
+		y_coord = int(line[line.find(",") + 1: -2])
+	  
+		if total_count % 2 == 1:
+		    
+		    index1 = np.ravel_multi_index((x_coord_prev + centre_coord_offset, y_coord_prev + centre_coord_offset), (brief_patch_size, brief_patch_size))
+		    index2 = np.ravel_multi_index((x_coord + centre_coord_offset, y_coord + centre_coord_offset), (brief_patch_size, brief_patch_size))
+		    
+		    weights[index1, output_count] = -1
+		    weights[index2, output_count] = 1
+
+		    output_count += 1
+
+		total_count += 1
+		
+	assert output_count == output_dimension, "The descriptor size should be " + str(output_dimension)
+
+	np.save('./weights/weights_dense_' + str(brief_version) + '.npy', weights)
+
+	file.close()
+	
+	return weights
+
+
+if __name__ == '__main__':
+	
+	generate_and_save_dense_layer_weights()
+
